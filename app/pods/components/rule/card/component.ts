87e8ff20494b968ebb33b0f0f2c5329ae46e54dd
Ilya Radchenko <knownasilya@gmail.com>
2021-07-14 12:10:37 -0400

    show source in  rule card

--- app/pods/components/rule/card/component.ts
+++ app/pods/components/rule/card/component.ts
@@ -4,14 +4,23 @@ import { action } from '@ember/object';
 import { task } from 'ember-concurrency';
 import Platform from 'mapgeo-sync/services/platform';
 import { tracked, cached } from '@glimmer/tracking';
-import { SyncRule } from 'mapgeo-sync-config';
+import { Source, SyncRule } from 'mapgeo-sync-config';
 
 interface RuleCardArgs {
   rule: SyncRule;
+  sources: Source[];
 }
 
 export default class RuleCard extends Component<RuleCardArgs> {
   @service('platform') declare platform: Platform;
+
+  @cached
+  get source() {
+    const source = this.args.sources.find(
+      (source) => source.id === this.args.rule.sourceId
+    );
+    return source;
+  }
 
   @task
   async runRule(rule: SyncRule) {
