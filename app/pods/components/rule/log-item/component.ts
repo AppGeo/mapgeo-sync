5598255880a5165f9158c08bc9fbfdcfcd9dea3e
Ilya Radchenko <knownasilya@gmail.com>
2021-08-03 11:47:55 -0400

    show file/select in edit, show duration in logs

--- app/pods/components/rule/log-item/component.ts
+++ app/pods/components/rule/log-item/component.ts
@@ -1,8 +1,14 @@
+import { helper } from '@ember/component/helper';
 import Component from '@glimmer/component';
+import { formatDistance } from 'date-fns';
 import { RuleLogItem } from 'mapgeo-sync-config';
 
 interface RuleLogItemArgs {
   item: RuleLogItem;
 }
 
-export default class RuleLogItemComponent extends Component<RuleLogItemArgs> {}
+export default class RuleLogItemComponent extends Component<RuleLogItemArgs> {
+  distance = helper(([log]: [RuleLogItem]) => {
+    return formatDistance(new Date(log.endedAt), new Date(log.startedAt));
+  });
+}
